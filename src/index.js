3b56b7178f3f2af0b6cc7c46b6c30d3724f8b181
JuanICasareski <juani.casareski@gmail.com>
2022-03-12 18:14:59 -0300

    Refact: Migrated to react-router v5.2.0

--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { 
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
-import ProductDescriptionPage from './routes/productDescriptionPage';
-
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import ProductDescriptionPage from './routes/productDescriptionPage'
+import ExampleComponent from './components/exampleComponent'
+import Navbar from './components/navbar'
 
 const rootElement = document.getElementById('root')
 
 ReactDOM.render(
   <Router>
-    <Routes>
-      <Route path='/' element={<h1>index</h1>} />
+    <Switch>
       <Route exact path="/item/:item_id" element={<ProductDescriptionPage />} />
-    </Routes>
+    </Switch>
   </Router>,
   rootElement
 );
