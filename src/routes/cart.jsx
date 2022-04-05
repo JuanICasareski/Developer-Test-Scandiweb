5dea51a16d555127a1fe90b4cf140b27082bcbb3
JuanICasareskii <juani.casareski@gmail.com>
2022-04-05 18:08:17 -0300

    Feat: Create basic cart page

---
+++ src/routes/cart.jsx
@@ -0,0 +1,34 @@
+import React, {Component} from 'react'
+import BigCartItem from '../components/bigCartItem'
+import CartContext from '../context/cartContext'
+import styles from '../components/styles/cart.scss'
+
+class Cart extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div className='cart'>
+                
+                <h1>CART</h1>
+
+                <div>
+                    {
+                        this.context.items && this.context.items.length !== 0?
+                            this.context.items.map((item, i) =>
+                                <BigCartItem item={item.itemInfo} selectedAttrs={item.selectedAttrs} count={item.count} order={i} />
+                            )
+                        :
+                            null
+                    }
+                </div>
+            </div>
+        )
+    }
+}
+
+Cart.contextType = CartContext
+
+export default Cart
