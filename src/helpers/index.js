316c4e60dcba5d500e75fc707a2ad80c68fcd8d9
JuanICasareski <juani.casareski@gmail.com>
2022-04-10 17:56:01 -0300

    Chore: Move PDP api query to helper file

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -48,3 +48,52 @@ export const getItemsFromCategory = (category) => {
 
     return data
 }
+
+export const getItemData = (itemId) => {
+    const query = `
+            query {
+                product(id:"${itemId}") {
+                    id
+                    name
+                    inStock
+                    gallery
+                    description
+                    category
+                    attributes {
+                        id
+                        name
+                        type
+                        items {
+                            displayValue
+                            value
+                            id
+                        }
+                    }
+                    prices {
+                        amount
+                        currency {
+                            label
+                            symbol
+                        }      
+                    }
+                    brand
+                }
+            }
+        `
+    const data = fetch("http://localhost:4000", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            query
+        })
+    }).then(response => {
+        return response.json()
+    }).then(data => {
+        return data.data.product
+    })
+
+    return data
+}
